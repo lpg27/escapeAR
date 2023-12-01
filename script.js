ec16670fb5964456cf4425f30ae06f5d21c709ee
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2023-12-01 14:23:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("Escape in 5 minutes, or you will meet your doom. Find the 16 digit code, and type it into the box to get out. Good luck!")
+alert("Escape in 5 minutes, or you will meet your doom. Find the 16 digit code, and type it into the box to get out. Drag to move. Good luck!")
 var code = "";
 for(let i = 0; i < 16; i++){
 	code += (Math.floor(Math.random()*10)).toString();
