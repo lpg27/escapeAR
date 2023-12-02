6be66c9d5c731443b8c6c6a61fce91c1fe2bd0ee
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2023-12-02 06:25:58 -0500

    Update script.js'

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.getElementById("note").onclick = ()=>{
 		speechSynthesis.speak(new SpeechSynthesisUtterance("Part 4 is: " + code.substring(12,16)));
 
 }
-function check(e){
+document.getElementById("escape").onclick = (e)=>{
 	if(e.key == "Enter"){
 		if(this.value === code){
 			alert("Great Job!");
