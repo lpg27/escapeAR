680c671c4f64e7f205e4ca8cd2385fdb3e46ecef
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2023-12-15 13:40:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ for(let i = 0; i < 16; i++){
 	code += (Math.floor(Math.random()*10)).toString();
 }
     // feature detect
-document.addEventListener("drag", (e)=>{
-	document.getElementById("mainDiv").style.transform = "translateZ(600px) rotateX("+(-e.clientY)+"deg) rotateY("+e.clientX+"deg)"
-})
+document.getElementById("mainDiv").onclick = ()=> {
+	navigator.permissions.query({ name: "deviceorientation" }).then((result) => {
+  if (result.state === "granted") {
+	  document.addEventListener("deviceorientation", (e)=> {
+		  document.getElementById("mainDiv").style.transform = "translateZ(600px) rotateZ("+ e.alpha+"deg) rotateX("+e.gamma+"deg) rotateY("+e.beta+"deg);"
+	  })
+  } else if (result.state === "prompt") {
+  }
+  // Don't do anything if the permission was denied.
+});
+}
 document.getElementById("retro"). onclick = ()=> {
 	speechSynthesis.speak(new SpeechSynthesisUtterance("Part 1 is: " + code.substring(0,4)));
 }
